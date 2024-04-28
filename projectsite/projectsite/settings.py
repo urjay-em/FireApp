561ff95a5ad87ca72479eb72c85b08ee01cfaece
Al-nizar Wahab <www.alnizarw07@gmail.com>
2024-04-28 12:15:16 +0800

    Allowed Host

--- projectsite/projectsite/settings.py
+++ projectsite/projectsite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-n#$o#^(@glm4=*(#t^(q(cay-f_&=v-pg0st=)^vyrk5#+3*9f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['zharfire.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
